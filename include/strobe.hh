6baa19d1af03f4183e45c6ec80d99d82bafcccde
jesse <jesse>
2017-07-03 22:18:59 -0400

    Increase flash intensity

--- include/strobe.hh
+++ include/strobe.hh
@@ -129,10 +129,10 @@ class Rectangle {
 public:
 
     Rectangle (double x1, double y1, double x2, double y2) : 
-        p1_(x1,y1), p2_(x2,y2), col_(mkcol(255,255,255,30)) {}
+        p1_(x1,y1), p2_(x2,y2), col_(mkcol(255,255,255,128)) {}
 
     Rectangle (const Point &p1, const Point &p2) : 
-        p1_(p1), p2_(p2), col_(mkcol(255,255,255,30)) {}
+        p1_(p1), p2_(p2), col_(mkcol(255,255,255,128)) {}
 
     inline void Draw () const {
         Draw (col_); 
